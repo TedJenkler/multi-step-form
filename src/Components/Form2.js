27416c84a84737ee8436e5f29addf5b2b3424423
TedJenkler <Tedjenkler@gmail.com>
2023-11-13 09:38:25 -0200

    page2 logic

--- src/Components/Form2.js
+++ src/Components/Form2.js
@@ -1,9 +1,12 @@
 import arcade from "../images/icon-arcade.svg"
 import advanced from "../images/icon-advanced.svg"
 import pro from "../images/icon-pro.svg"
+import { useState } from "react"
 
 const Form2 = (props) => {
+    const [cart, setCart] = useState("")
 
+    console.log(cart)
     const handlesubmit = (e) => {
         e.preventDefault()
         props.setPage(3)
@@ -12,36 +15,37 @@ const Form2 = (props) => {
     const stoprefresh = (e) => {
         e.preventDefault()
     }
+
 return(<>
     <form onSubmit={stoprefresh}>
     <div className="form-inputpart">
         <h1>Select your plan</h1>
         <p>You have the option of monthly or yearly billing.</p>
-        <button className="price">
+        <button onClick={(e) => {setCart("arcade")}} value={cart} className="price">
             <img src={arcade} alt="Arcade"></img>
             <div>
                 <p>Arcade</p>
                 <p>$9/mo</p>
             </div>
         </button>
-        <button className="price">
+        <button onClick={(e) => {setCart("advanced")}} value={cart} className="price">
             <img src={advanced} alt="Advanced"></img>
             <div>
                 <p>Advanced</p>
                 <p>$12/mo</p>
             </div>
         </button>
-        <button className="price">
+        <button onClick={(e) => {setCart("pro")}} value={cart} className="price">
             <img src={pro} alt="Pro"></img>
             <div>
                 <p>Pro</p>
                 <p>$15/mo</p>
             </div>
         </button>
-    <div class="form-check form-switch checkarea">
-        <label class="form-check-label label-left" for="flexSwitchCheckDefault">Monthly</label>
-        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-        <label class="form-check-label label-right" for="flexSwitchCheckDefault">Yearly</label>
+    <div className="form-check form-switch checkarea">
+        <label className="form-check-label float-left" htmlFor="flexSwitchCheckDefault">Monthly</label>
+        <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+        <label className="form-check-label float-right" htmlFor="flexSwitchCheckDefault">Yearly</label>
         </div>
     </div>
         <div className="form-submitpart">
