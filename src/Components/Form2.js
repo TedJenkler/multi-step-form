f66fe22599380b109dc0a9cb4d3ab0140c9ba471
TedJenkler <Tedjenkler@gmail.com>
2023-11-13 09:00:38 -0200

    page 2 css

--- src/Components/Form2.js
+++ src/Components/Form2.js
@@ -1,21 +1,48 @@
+import arcade from "../images/icon-arcade.svg"
+import advanced from "../images/icon-advanced.svg"
+import pro from "../images/icon-pro.svg"
+
 const Form2 = (props) => {
 
     const handlesubmit = (e) => {
         e.preventDefault()
         props.setPage(3)
     }
+
+    const stoprefresh = (e) => {
+        e.preventDefault()
+    }
 return(<>
-    <form onSubmit={handlesubmit}>
+    <form onSubmit={stoprefresh}>
     <div className="form-inputpart">
-        <h1>Personal Info</h1>
-        <p> Please provide your name, email address, and phone number.</p>
-        <label htmlFor="">Name</label>
-        <input onChange={(e) => {props.setName(e.target.value)}} value={props.name} placeholder="e.g. Stephen King"></input>
-        {console.log(props.email)}
-        <label htmlFor="">Email Address</label>
-        <input onChange={(e) => {props.setEmail(e.target.value)}} value={props.email} placeholder="e.g. stephenking@lorem.com"></input>
-        <label htmlFor="">Phone Number</label>
-        <input onChange={(e) => {props.setPhone(e.target.value)}} value={props.phone} placeholder="+1 234 567 890"></input>
+        <h1>Select your plan</h1>
+        <p>You have the option of monthly or yearly billing.</p>
+        <button className="price">
+            <img src={arcade} alt="Arcade"></img>
+            <div>
+                <p>Arcade</p>
+                <p>$9/mo</p>
+            </div>
+        </button>
+        <button className="price">
+            <img src={advanced} alt="Advanced"></img>
+            <div>
+                <p>Advanced</p>
+                <p>$12/mo</p>
+            </div>
+        </button>
+        <button className="price">
+            <img src={pro} alt="Pro"></img>
+            <div>
+                <p>Pro</p>
+                <p>$15/mo</p>
+            </div>
+        </button>
+    <div class="form-check form-switch checkarea">
+        <label class="form-check-label label-left" for="flexSwitchCheckDefault">Monthly</label>
+        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+        <label class="form-check-label label-right" for="flexSwitchCheckDefault">Yearly</label>
+        </div>
     </div>
         <div className="form-submitpart">
             <button onClick={handlesubmit}>Next Step</button>
