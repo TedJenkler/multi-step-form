3be4d4826813d53c896711a125e1c2b82329f4db
TedJenkler <Tedjenkler@gmail.com>
2023-11-15 08:16:21 -0200

    page 1 validation - double click bug

--- src/Components/Form1.js
+++ src/Components/Form1.js
@@ -1,24 +1,44 @@
-
-
+import { useState } from "react";
 
 const Form1 = (props) => {
+    const [vName, setVName] = useState("Before input")
+    const [vEmail, setVEmail] = useState("Before input")
+    const [vPhone, setVPhone] = useState("Before input")
+
+    const validator = () => {
+        props.name.length >= 3 ? setVName(true) : setVName(false);
+        props.email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) ? setVEmail(true) : setVEmail(false);
+        props.phone.match(/^[0-9 ()+/-]{5,}$/) ? setVPhone(true) : setVPhone(false);
+    }
 
     const handlesubmit = (e) => {
+        validator()
         e.preventDefault()
-        props.setPage(2)
+        if(vName === true && vEmail === true && vPhone === true){
+            console.log("tf")
+            props.setPage(2)
+        }
     }
 return(<>
     <form onSubmit={handlesubmit}>
     <div className="form-inputpart">
         <h1>Personal Info</h1>
         <p> Please provide your name, email address, and phone number.</p>
-        <label htmlFor="">Name</label>
-        <input onChange={(e) => {props.setName(e.target.value)}} value={props.name} placeholder="e.g. Stephen King"></input>
-        {console.log(props.email)}
-        <label htmlFor="">Email Address</label>
-        <input onChange={(e) => {props.setEmail(e.target.value)}} value={props.email} placeholder="e.g. stephenking@lorem.com"></input>
-        <label htmlFor="">Phone Number</label>
-        <input onChange={(e) => {props.setPhone(e.target.value)}} value={props.phone} placeholder="+1 234 567 890"></input>
+        <div className="labelgroup">
+            <label htmlFor="name">Name</label>
+            <span id={vName === false ? "error" : ""} >This field is required</span>
+        </div>
+        <input className={vName === false ? "errorinput" : null } onChange={(e) => {props.setName(e.target.value)}} value={props.name} name="name" placeholder="e.g. Stephen King"></input>
+        <div className="labelgroup">
+            <label htmlFor="email">Email</label>
+            <span id={vEmail === false ? "error" : ""} >This field is required</span>
+        </div>
+        <input className={vEmail === false ? "errorinput" : null } name="email" type="email" onChange={(e) => {props.setEmail(e.target.value)}} value={props.email} placeholder="e.g. stephenking@lorem.com"></input>
+        <div className="labelgroup">
+            <label htmlFor="phone">Phone</label>
+            <span id={vPhone === false ? "error" : ""} >This field is required</span>
+        </div>
+        <input className={vPhone === false ? "errorinput" : null } name="phone" type="phone"  onChange={(e) => {props.setPhone(e.target.value)}} value={props.phone} placeholder="+1 234 567 890"></input>
     </div>
         <div className="form-submitpart">
             {false ? <button className="backbtn">Go Back</button> : <button className="backbtn hidden">Go Back</button> }
--- src/Components/Pages.js
+++ src/Components/Pages.js
@@ -6,7 +6,7 @@ return(
             {props.page == 1 ? <span id="i1-active">1</span> : <span id="i1">1</span> }
             {props.page == 2 ? <span id="i2-active">2</span> : <span id="i2">2</span> }
             {props.page == 3 ? <span id="i3-active">3</span> : <span id="i3">3</span> }
-            {props.page == 4 || 5 ? <span id="i4-active">4</span> : <span id="i4">4</span> }
+            {props.page >= 4 ? <span id="i4-active">4</span> : <span id="i4">4</span> }
         </div>
     </div>
 )
