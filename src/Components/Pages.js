40b1132d4c05e443b9133a424cd8aa802d02b4d6
TedJenkler <Tedjenkler@gmail.com>
2023-11-16 10:25:21 -0200

    fully responsive - 2 bugs left

--- src/Components/Pages.js
+++ src/Components/Pages.js
@@ -8,6 +8,34 @@ return(
             {props.page == 3 ? <span id="i3-active">3</span> : <span id="i3">3</span> }
             {props.page >= 4 ? <span id="i4-active">4</span> : <span id="i4">4</span> }
         </div>
+        <div className="responsive-pageindex">
+            {props.page == 1 ? <span id="i1-active">1</span> : <span id="i1">1</span> }
+            <div className="responsive-pagecomponent">
+                <p>Step 1</p>
+                <p className="whiteboldtext">YOUR INFO</p>
+            </div>
+        </div>
+        <div className="responsive-pageindex">
+            {props.page == 2 ? <span id="i2-active">2</span> : <span id="i2">2</span> }
+            <div className="responsive-pagecomponent">
+                <p>Step 2</p>
+                <p className="whiteboldtext" >SELECT PLAN</p>
+            </div>
+        </div>
+        <div className="responsive-pageindex">
+            {props.page == 3 ? <span id="i3-active">3</span> : <span id="i3">3</span> }
+            <div className="responsive-pagecomponent">
+                <p>Step 3</p>
+                <p className="whiteboldtext">ADD-ONS</p>
+            </div>
+        </div>
+        <div className="responsive-pageindex">
+            {props.page >= 4 ? <span id="i4-active">4</span> : <span id="i4">4</span> }
+            <div className="responsive-pagecomponent">
+                <p>Step 4</p>
+                <p className="whiteboldtext">SUMMARY</p>
+            </div>
+        </div>
     </div>
 )
 }
